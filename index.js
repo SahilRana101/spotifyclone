0c84e2fb3e01d426fbd7e7c0fa7d7a2fd5fdf68b
Sahil Rana <rana1.sahil9.19@gmail.com>
2022-09-11 16:40:42 +0530

    Made required changes

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var count1 = 0;
 var music_playlist = []
 var ID = 0
 
-var music_list = ['songs/Legend.mp3','songs/Blinding Lights.mp3','songs/295.mp3','songs/Unforgettable.mp3',
-'songs/Sidhu Son.mp3','songs/Bewafa.mp3','songs/Starboy.mp3','songs/One Dance.mp3',
-'songs/Bad Guy.mp3','songs/Brown Munde.mp3','songs/Proper Patola.mp3','songs/440 Volt.mp3',
-'songs/Mask Off.mp3','songs/Less than zero.mp3'];
+var music_list = {
+    1:["Legend","Sidhu Moosewala", 'songs/Legend.mp3', "images/1.jpeg"],
+    2:["Blinding Lights","The Weeknd", 'songs/Blinding Lights.mp3', "images/weekndimg.jpeg"],
+    3:["295","Sidhu Moosewala", 'songs/295.mp3', "images/295.jpeg"],
+    4:["Unforgettable","Diljit Dosanjh", 'songs/Unforgettable.mp3', "images/unforgettable.jpeg"],
+    5:["Sidhu Son","Sidhu Moosewala", 'songs/Sidhu Son.mp3', "images/sidhuson.jpeg"],
+    6:["Bewafa","Imran Khan", 'songs/Bewafa.mp3', "images/bewafa.jpeg"],
+    7:["Starboy","The Weeknd", 'songs/Starboy.mp3', "images/starboy.jpeg"],
+    8:["One Dance","Drake", 'songs/One Dance.mp3', "images/onedance.jpeg"],
+    9:["Bad Guy","Billie Eilish", 'songs/Bad Guy.mp3', "images/badguy.jpeg"],
+    10:["Brown Munde","Ap Dhillon", 'songs/Brown Munde.mp3', "images/brownmunde.jpeg"],
+    11:["Proper Patola","Diljit Dosanjh", 'songs/Proper Patola.mp3', "images/proper.jpeg"],
+    12:["440 Volt","Diljit Dosanjh", 'songs/440 Volt.mp3', "images/440.jpeg"],
+    13:["Mask Off","Future", 'songs/Mask Off.mp3', "images/maskoff.jpeg"],
+    14:["Less than zero","The Weeknd", 'songs/Less than zero.mp3', "images/zero.jpeg"]
+}
 
 
 function barPlayButton(){
@@ -58,7 +70,7 @@ function playTheSong(id){
     if (ID == 0) newPlay(id);
     else pauseAndPlay(id, ID);
 
-    console.log("2  " + id + "  " + ID + "  " + music_list[id-1]);
+    console.log("2  " + id + "  " + ID + "  " + music_list[id][2]);
 }
 
 
@@ -78,9 +90,9 @@ function pause(id){
 function newPlay(id){
     // new_play function will play the song.
 
-    const music = new Audio(music_list[id-1]);
+    const music = new Audio(music_list[id][2]);
     music_playlist.unshift(music);
-    console.log(music_list[id-1]);
+    console.log(music_list[id][2]);
     music.play();
     music.loop =true;
 
@@ -98,9 +110,9 @@ function pauseAndPlay(id){
     music_playlist[0].pause();
     document.getElementById("play").className = "bi bi-play-fill";
     music_playlist.shift(0);
-    const music = new Audio(music_list[id-1]);
+    const music = new Audio(music_list[id][2]);
     music_playlist.unshift(music);
-    console.log(music_list[id-1]);
+    console.log(music_list[id][2]);
     music.play();
     music.loop =true;
 
@@ -149,123 +161,11 @@ function playPreviousSong(){
 }
 
 
-function changeImageTitleSubtitle(){
-
+function changeImageTitleSubtitle(song_id){
+    document.getElementById("title").innerHTML = music_list[song_id][0];
+    document.getElementById("poster_play").src = music_list[song_id][3];
+    document.getElementById("16").textContent = music_list[song_id][1];
 }
 
 
 function isPlaying(song) { return !song.paused; }
-
-
-/*const songs = [
-    {
-        id:'1',
-        songName:` legend <br>
-        <div class="subtitle">Sidhu moosewala</div>`,
-        poster:"images/1.jpeg"
-
-    },
-    {
-        id:'2',
-        songName:` Blindings Light <br>
-        <div class="subtitle">The Weeknd</div>`,
-        poster:"images/weekndimg.jpg"
-
-    },
-    {
-        id:'3',
-        songName:` 295 <br>
-        <div class="subtitle">Sidhu Moosewala</div>`,
-        poster:"images/295.jpg"
-
-    },
-    {
-        id:'4',
-        songName:` Unforgettable <br>
-        <div class="subtitle">Diljit Dosanjh</div>`,
-        poster:"images/unforgettable.jpg"
-    },
-    {
-        id:'5',
-        songName:` Sidhu Son<br>
-        <div class="subtitle">Sidhu Moosewala</div>`,
-        poster:"images/sidhuson.jpg"
-    },
-    {
-        id:'6',
-        songName:` Bewafa <br>
-        <div class="subtitle">Imran khan</div>`,
-        poster:"images/bewafa.jpg"
-    },
-    {
-        id:'7',
-        songName:` Starboy <br>
-        <div class="subtitle">The Weeknd</div>`,
-        poster:"images/starboy.jpg"
-    },
-    {
-        id:'8',
-        songName:` one dance <br>
-        <div class="subtitle">Drake</div>`,
-        poster:"images/onedance.jpg"
-    },
-    {
-        id:'9',
-        songName:` Bad Guy <br>
-        <div class="subtitle">Billie Eilish</div>`,
-        poster:"images/badguy.jpg"
-    },
-    {
-        id:'10',
-        songName:` Brown Munde <br>
-        <div class="subtitle">Ap Dhillon</div>`,
-        poster:"images/brownmunde.jpg"
-    },
-    {
-        id:'11',
-        songName:` Proper Patola <br>
-        <div class="subtitle">Diljit Dosanjh</div>`,
-        poster:"images/proper.jpg"
-    },
-    {
-        id:'12',
-        songName:` 440 volt <br>
-        <div class="subtitle">Mika singh</div>`,
-        poster:"images/440.jpg"
-    },
-    {
-        id:'13',
-        songName:` Mask Off <br>
-        <div class="subtitle">Future</div>`,
-        poster:"images/maskoff.jpg"
-    },
-    {
-        id:'14',
-        songName:` Less than zero <br>
-        <div class="subtitle">The Weeknd</div>`,
-        poster:"images/zero.jpg"
-    }
-    
-]
-    Array.from(document.getElementsByClassName('songItem')).forEach((element,i) =>{
-    element.getElementsByTagName('img')[0].src = songs[i].poster;
-    element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
-
-})*/
-
-/*
-let play = document.getElementById('play');
-
-play.addEventListener('click', ()=>{
-    if(music.paused || music.currentTime <=0){
-        music.play();
-        play.classList.remove('bi-play-fill');
-        play.classList.add('bi-pause-fill');
-    } else{
-        music.pause();
-        play.classList.add('bi-play-fill');
-        play.classList.remove('bi-pause-fill');
-    }
-})*/
-
-
